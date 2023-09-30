ba951166794d4c5cc9b8e000d0e0cf5c676130d2
Antonin <antonin.pire@gmail.com>
2023-09-30 13:44:18 +0200

    add db indexes on projects and columns

--- src/lib/db/schema/columns.ts
+++ src/lib/db/schema/columns.ts
@@ -1,26 +1,32 @@
 /* eslint-disable no-relative-import-paths/no-relative-import-paths */
 import { createId } from "@paralleldrive/cuid2";
 import { relations, type InferSelectModel } from "drizzle-orm";
-import { smallint, timestamp, varchar } from "drizzle-orm/mysql-core";
+import { index, smallint, timestamp, varchar } from "drizzle-orm/mysql-core";
 import { mysqlTable } from "../mysql-table";
 import { projects } from "./projects";
 import { tasks } from "./tasks";
 
-export const columns = mysqlTable("columns", {
-  id: varchar("id", { length: 30 })
-    .primaryKey()
-    .$defaultFn(() => `col_${createId()}`),
-  name: varchar("name", { length: 75 }).notNull(),
-  projectId: varchar("projectId", { length: 28 }).notNull(),
-  order: smallint("order").notNull().default(0),
-  color: varchar("color", { length: 7 }).notNull(),
-  updatedAt: timestamp("updatedAt")
-    .notNull()
-    .$defaultFn(() => new Date()),
-  createdAt: timestamp("createdAt")
-    .notNull()
-    .$defaultFn(() => new Date()),
-});
+export const columns = mysqlTable(
+  "columns",
+  {
+    id: varchar("id", { length: 30 })
+      .primaryKey()
+      .$defaultFn(() => `col_${createId()}`),
+    name: varchar("name", { length: 75 }).notNull(),
+    projectId: varchar("projectId", { length: 28 }).notNull(),
+    order: smallint("order").notNull().default(0),
+    color: varchar("color", { length: 7 }).notNull(),
+    updatedAt: timestamp("updatedAt")
+      .notNull()
+      .$defaultFn(() => new Date()),
+    createdAt: timestamp("createdAt")
+      .notNull()
+      .$defaultFn(() => new Date()),
+  },
+  (table) => ({
+    projectIds: index("project_idx").on(table.projectId),
+  })
+);
 
 export const columnsRelations = relations(columns, ({ one, many }) => ({
   project: one(projects, {
--- src/lib/db/schema/tasks.ts
+++ src/lib/db/schema/tasks.ts
@@ -2,6 +2,7 @@
 import { createId } from "@paralleldrive/cuid2";
 import { relations, type InferSelectModel } from "drizzle-orm";
 import {
+  index,
   mysqlEnum,
   smallint,
   text,
@@ -13,22 +14,28 @@ import { columns } from "./columns";
 import { subTasks } from "./sub-tasks";
 import { tagsByTasks } from "./tags-by-tasks";
 
-export const tasks = mysqlTable("tasks", {
-  id: varchar("id", { length: 30 })
-    .primaryKey()
-    .$defaultFn(() => `tsk_${createId()}`),
-  title: varchar("title", { length: 75 }).notNull(),
-  description: text("description"),
-  order: smallint("order").notNull().default(0),
-  priority: mysqlEnum("priority", ["low", "medium", "high"]),
-  columnId: varchar("columnId", { length: 30 }).notNull(),
-  updatedAt: timestamp("updatedAt")
-    .notNull()
-    .$defaultFn(() => new Date()),
-  createdAt: timestamp("createdAt")
-    .notNull()
-    .$defaultFn(() => new Date()),
-});
+export const tasks = mysqlTable(
+  "tasks",
+  {
+    id: varchar("id", { length: 30 })
+      .primaryKey()
+      .$defaultFn(() => `tsk_${createId()}`),
+    title: varchar("title", { length: 75 }).notNull(),
+    description: text("description"),
+    order: smallint("order").notNull().default(0),
+    priority: mysqlEnum("priority", ["low", "medium", "high"]),
+    columnId: varchar("columnId", { length: 30 }).notNull(),
+    updatedAt: timestamp("updatedAt")
+      .notNull()
+      .$defaultFn(() => new Date()),
+    createdAt: timestamp("createdAt")
+      .notNull()
+      .$defaultFn(() => new Date()),
+  },
+  (table) => ({
+    columnIdx: index("column_idx").on(table.columnId),
+  })
+);
 
 export const tasksRelations = relations(tasks, ({ one, many }) => ({
   column: one(columns, {
